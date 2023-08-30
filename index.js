6d6bf22030d55063e8269d60a1bdc068fc380a24
Kelly Reyes <kellyreyes219@gmail.com>
2023-08-30 18:28:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+
+  const cats = ["Milo", "Otis", "Garfield"]
+
+  function destructivelyAppendCat(name) {
+    cats.push(name)
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name)
+
+  }
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+function destructivelyRemoveFirstCat() { 
+  cats.shift()
+}
+function appendCat(name) { 
+  const newarray = cats.slice()
+  newarray.push(name)
+  return newarray
+}
+function prependCat(name) {
+  const secarray = cats.slice()
+  secarray.unshift(name)
+  return secarray
+}
+function removeLastCat() {
+  const thirdarray = cats.slice()
+  thirdarray.pop(1) 
+  return thirdarray
+}
+function removeFirstCat() {
+  const fourtharray = cats.slice() 
+  fourtharray.shift(1)
+  return fourtharray
+   
+}
+
+
+
